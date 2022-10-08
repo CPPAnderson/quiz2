aa1909d2e9838f23e2f13ea3c67c430368148d1d
ChristianA <canderson@cpp.edu>
2022-10-07 23:16:08 -0700

    added a construst that will print the TTT table.

--- ticTacToe.c
+++ ticTacToe.c
@@ -10,6 +10,18 @@ char letter = 'X';
 int y, x, cx, cy;
 time_t t;
 // a contruct that first clears the screen of previous code and then print out the TTT board
+void printScreen(){
+    system("cls");
+    printf("    1   2   3   \n");
+    printf("  +-----------+ \n");
+    printf("1 | %c | %c | %c | \n", a1, a2, a3);
+    printf("  +-----------+ \n");
+    printf("2 | %c | %c | %c | \n", b1, b2, b3);
+    printf("  +-----------+ \n");
+    printf("3 | %c | %c | %c | \n", c1, c2, c3);
+    printf("  +-----------+ \n");
+
+}
 
 // a contruct that makes sure that the spot the player is trying to move to has not been already taken or also with range
 
