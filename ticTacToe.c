49474d4c0dd3aaeb386012e73294f48f50c4ed55
ChristianA <canderson@cpp.edu>
2022-10-07 23:04:27 -0700

    Step 1 of quiz. inital file with comments on what my plan is.

---
+++ ticTacToe.c
@@ -0,0 +1,52 @@
+//Tic Tac Toe game between 2 players or player vs computer
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h> 
+#include <time.h> 
+
+// all global variables that will be needed for the TTT program
+
+// a contruct that first clears the screen of previous code and then print out the TTT board
+
+// a contruct that makes sure that the spot the player is trying to move to has not been already taken or also with range
+
+// if player chooses a slot that is already taken, program ask the player to re-enter for a new spot.
+
+// if player enters a x and y value that is out of range of the board, this construct should ask him to re-enter there x and y
+
+// A construct that changes letters on the board
+
+// A construct that returns true if the game is over with a winner.
+
+// a construct that generates a random number
+
+// a construct that makes sure the computer is making valid moves
+
+int main(){
+
+    //prompt user for game they wish to play 
+    
+    switch(){
+        case (1):
+            //player vs player
+            while (true){
+                
+                // asks player 1 for there X and Y 
+
+                // asks player 2 for there x and y
+            }
+            break;
+        case (2):
+            //player vs computer
+            while (true){ 
+
+                //asks player for there x and y
+
+                // code for computers side
+
+            }
+            break;
+    }
+
+    return 0;
+}
